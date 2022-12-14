44a0ee3ddbe5d1c2247b8942aa6336be07da7ed3
MTTHSVSS <matthias@mtthsvss.com>
2022-12-14 14:12:15 +0100

    I tried to clone this repo and get it to run locally.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/vossmatthias/Desktop/ai/openai-quickstart-python/venv"
+set -gx VIRTUAL_ENV "/Users/vossmatthias/Desktop/ai/namegenerator/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
